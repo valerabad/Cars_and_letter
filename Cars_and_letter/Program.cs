3c1e7e6fdac6c0e615fc4ad0284ee60347b82481
Valeriy Baditsa <valerabaditsa@gmail.com>
2022-09-21 15:23:43 +0300

    Initial commit

---
+++ Cars_and_letter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cars_and_letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
